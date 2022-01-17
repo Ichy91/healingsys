8784a5f73877e312d10586db044782a7a641a65b
Ichy91 <farago.istvan91@gmail.com>
2022-01-17 11:20:35 +0100

    setup the project

---
+++ src/test/java/com/healingsys/HealingsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healingsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
