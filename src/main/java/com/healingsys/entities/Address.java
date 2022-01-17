8939c987596257115db8167bcc00e22bb088c7e7
Ichy91 <farago.istvan91@gmail.com>
2022-01-17 15:44:26 +0100

    feat: add set up to the Address entity

--- src/main/java/com/healingsys/entities/Address.java
+++ src/main/java/com/healingsys/entities/Address.java
@@ -1,4 +1,41 @@
 package com.healingsys.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Locale;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
+
+    @Id
+    @GeneratedValue
+    @GenericGenerator(name="postgres-uuid", strategy = "uuid")
+    private UUID id;
+
+    @NotNull
+    private Locale country;
+
+    @NotNull
+    private String postalCode;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String street;
+
+    @NotNull
+    private String houseNumber;
+
 }
