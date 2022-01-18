1d4d1b2a301e5bdbf4686ae085de61961c292ffd
Ichy91 <farago.istvan91@gmail.com>
2022-01-18 12:43:45 +0100

    feat: add implementation to the Appointment entity

--- src/main/java/com/healingsys/entities/Appointment.java
+++ src/main/java/com/healingsys/entities/Appointment.java
@@ -1,4 +1,35 @@
 package com.healingsys.entities;
 
+import com.healingsys.entities.enums.AppointmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @NotNull
+    private LocalDate date;
+
+    @NotNull
+    private String hour;
+
+    @Enumerated(EnumType.STRING)
+    private AppointmentStatus status;
+
 }
