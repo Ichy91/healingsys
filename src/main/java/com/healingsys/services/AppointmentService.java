ec74b52f688f1c0c60d9d22e26dea5d086175fde
Ichy91 <farago.istvan91@gmail.com>
2022-01-20 21:03:01 +0100

    feat: add new method for getting the operation details

--- src/main/java/com/healingsys/services/AppointmentService.java
+++ src/main/java/com/healingsys/services/AppointmentService.java
@@ -1,4 +1,24 @@
 package com.healingsys.services;
 
+import com.healingsys.entities.OperationDetails;
+import com.healingsys.repositories.AppointmentRepository;
+import com.healingsys.repositories.DayOfWeekRepository;
+import com.healingsys.repositories.OperationDetailsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class AppointmentService {
+    private final AppointmentRepository appointmentRepository;
+    private final OperationDetailsRepository operationDetailsRepository;
+    private final DayOfWeekRepository dayOfWeekRepository;
+
+
+    public Optional<OperationDetails> getOperationDetails() {
+        long id = 1;
+        return operationDetailsRepository.findById(id);
+    }
 }
