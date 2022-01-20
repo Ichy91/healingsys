43214e1fca4966b9f22fe09a92e7bb5655740c3a
Ichy91 <farago.istvan91@gmail.com>
2022-01-20 21:06:53 +0100

    feat: add implementation for principal generation of appointments

--- src/main/java/com/healingsys/services/AppointmentManagerService.java
+++ src/main/java/com/healingsys/services/AppointmentManagerService.java
@@ -1,22 +1,48 @@
 package com.healingsys.services;
 
+import com.healingsys.entities.OperationDetails;
 import com.healingsys.util.Day;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 @Data
 @RequiredArgsConstructor
 public class AppointmentManagerService {
-
     private final AppointmentService appointmentService;
     private List<Day> days;
-
-    public void daisHandler(LocalDate toDayDate) {}
-
-    private void setupDays(LocalDate toDayDate) {}
+    private OperationDetails details;
 
 
+    public List<Day> appointmentHandler(LocalDateTime toDayDateTime) {
+        details = appointmentService.getOperationDetails().get();
+
+        if (days == null || days.isEmpty()) setupDays(toDayDateTime);
+
+        return getDays();
+    }
+
+    private void setupDays(LocalDateTime toDayDateTime) {
+        days = new ArrayList<>();
+        LocalDate today = toDayDateTime.toLocalDate();
+
+        while (days.size() < details.getMaxGeneratedDays()) {
+            if (!details.getClosedDay().toString().contains(today.getDayOfWeek().toString())) {
+                generateDay(today);
+            }
+            today = today.plusDays(1);
+        }
+    }
+
+    private void generateDay(LocalDate today) {
+        Day actualDay = new Day(today, details);
+        actualDay.dayHandler();
+        days.add(actualDay);
+    }
 }
--- src/main/java/com/healingsys/util/Day.java
+++ src/main/java/com/healingsys/util/Day.java
@@ -1,10 +1,15 @@
 package com.healingsys.util;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.healingsys.entities.OperationDetails;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -12,6 +17,45 @@ import java.util.List;
 @NoArgsConstructor
 public class Day {
 
+    @JsonIgnore
+    private OperationDetails details;
     private LocalDate day;
     private List<Slot> slots;
+
+    public Day(LocalDate day, OperationDetails details) {
+        this.day = day;
+        this.details = details;
+    }
+
+
+    public void dayHandler() {
+        if (slots == null || slots.isEmpty()) setupSlots();
+    }
+
+    private void setupSlots() {
+        slots = new ArrayList<>();
+
+        int slotsNumberOfDay = calculateNumberOfSlots();
+        long slotLengthInMinute = (long) (details.getSlotLengthInHour() * 60);
+        LocalTime time = details.getOpen();
+
+        for (int i = 0; i < slotsNumberOfDay; i++) {
+            slots.add(generateSlot(details.getSlotMaxCapacity(), time));
+            time = time.plusMinutes(slotLengthInMinute);
+        }
+    }
+
+    private Slot generateSlot(int capacity, LocalTime time) {
+        Slot actualSlot = new Slot();
+        actualSlot.setCapacity(capacity);
+        actualSlot.setTime(time);
+        return actualSlot;
+    }
+
+    private int calculateNumberOfSlots() {
+        long openHours = ChronoUnit.HOURS.between(details.getOpen(), details.getClosed());
+        return (int) Math.floor(openHours / details.getSlotLengthInHour());
+    }
+
+
 }
