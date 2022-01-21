5f0a6c0bf6e5b92df88e72a69e70cebb5137d028
Ichy91 <farago.istvan91@gmail.com>
2022-01-21 13:51:56 +0100

    feat: add method for getting slots with parameters

--- src/main/java/com/healingsys/services/AppointmentService.java
+++ src/main/java/com/healingsys/services/AppointmentService.java
@@ -1,12 +1,17 @@
 package com.healingsys.services;
 
+import com.healingsys.entities.Appointment;
 import com.healingsys.entities.OperationDetails;
+import com.healingsys.entities.enums.AppointmentStatus;
 import com.healingsys.repositories.AppointmentRepository;
 import com.healingsys.repositories.DayOfWeekRepository;
 import com.healingsys.repositories.OperationDetailsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,5 +25,9 @@ public class AppointmentService {
     public Optional<OperationDetails> getOperationDetails() {
         long id = 1;
         return operationDetailsRepository.findById(id);
+    }
+
+    public List<Appointment> getReservedAppointmentsByDayAndHour(LocalDate day, LocalTime time) {
+        return appointmentRepository.findAllByDateAndHourAndStatus(day, time, AppointmentStatus.RESERVED);
     }
 }
