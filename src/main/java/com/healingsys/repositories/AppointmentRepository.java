0ee7cc6ea4fd7d7e759844a343d0961eac07d6f3
Ichy91 <farago.istvan91@gmail.com>
2022-01-21 13:51:39 +0100

    feat: add method for getting slots with parameters

--- src/main/java/com/healingsys/repositories/AppointmentRepository.java
+++ src/main/java/com/healingsys/repositories/AppointmentRepository.java
@@ -1,9 +1,16 @@
 package com.healingsys.repositories;
 
 import com.healingsys.entities.Appointment;
+import com.healingsys.entities.enums.AppointmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment> findAllByDateAndHourAndStatus(LocalDate day, LocalTime time, AppointmentStatus status);
 }
