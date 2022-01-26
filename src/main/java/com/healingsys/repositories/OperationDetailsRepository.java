29a113780725cc5b352a625a66f85b68f2bd4d18
Ichy91 <farago.istvan91@gmail.com>
2022-01-26 15:15:06 +0100

    feat: add new method for find Operations by Status

--- src/main/java/com/healingsys/repositories/OperationDetailsRepository.java
+++ src/main/java/com/healingsys/repositories/OperationDetailsRepository.java
@@ -1,9 +1,14 @@
 package com.healingsys.repositories;
 
 import com.healingsys.entities.OperationDetails;
+import com.healingsys.entities.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OperationDetailsRepository extends JpaRepository<OperationDetails, Long> {
+
+    public List<OperationDetails> findAllByStatus(Status status);
 }
