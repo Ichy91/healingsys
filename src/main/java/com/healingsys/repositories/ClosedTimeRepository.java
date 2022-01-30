ef3076af0aa1ce8cff7269dae03bca842d3b96e2
Ichy91 <farago.istvan91@gmail.com>
2022-01-30 01:06:58 +0100

    feat: add special methods for find ClosedTime

--- src/main/java/com/healingsys/repositories/ClosedTimeRepository.java
+++ src/main/java/com/healingsys/repositories/ClosedTimeRepository.java
@@ -4,10 +4,18 @@ import com.healingsys.entities.ClosedTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Repository
 public interface ClosedTimeRepository extends JpaRepository<ClosedTime, Long> {
 
-    List<ClosedTime> findAllByDepartmentDetailsId(Long id);
+    List<ClosedTime> findAllByDepartmentDetailsId(Long departmentId);
+
+    List<ClosedTime> findAllByDepartmentDetailsIdAndDateGreaterThanEqualOrderByDate(Long departmentId, LocalDate date);
+
+    List<ClosedTime> findAllByDepartmentDetailsIdAndDateAndClosedFromAndClosedTo(Long departmentId, LocalDate date, LocalTime closedFrom, LocalTime closedTo);
+
+    List<ClosedTime> findAllByDepartmentDetailsIdAndDate(Long departmentId, LocalDate date);
 }
